f41014e03788aba5ebc3cc9c9d312f95705e4a1a
campaigner0210 <86864592+campaigner0210@users.noreply.github.com>
2022-06-29 20:16:37 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,107 @@
+import sys
+import os
+import json
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from garbage import Ui_GarbageSortingUI
+import torch
+from torchvision import models
+import torch.nn as nn
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
+
+
+class Main(QMainWindow, Ui_GarbageSortingUI):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.setupUi(self)
+        self.ProjectPath = os.getcwd() + '/垃圾图片库'  # 获取当前工程文件位置
+        self.imgName = ''
+        self.ture_type = ''
+        self.pre_type = ''
+        self.jpg = ''
+        self.labels = self.label()
+        self.img = ''
+        self.model = self.get_model()
+        self.val_tf = transforms.Compose([
+            transforms.Resize(160),
+            transforms.ToTensor(),
+        ])
+        self.Input.clicked.connect(self.openImage)
+        self.Output.clicked.connect(self.calssify)
+
+    def label(self):
+        with open('dir_label.txt', 'r', encoding='utf-8') as f:
+            labels = f.readlines()
+            labels = list(map(lambda x: x.strip().split('\t'), labels))
+            return labels
+
+    def openImage(self):
+        self.imgName, imgType = QFileDialog.getOpenFileName(None, "打开图片", self.ProjectPath, "All Files(*)")
+        self.ture_type = self.imgName.split('/')[-2]
+        self.jpg = QtGui.QPixmap(self.imgName).scaled(self.TestImage.width(), self.TestImage.height())
+        self.img = Image.open(self.imgName)
+        self.img = self.img.convert('RGB')  # 去除透明通道
+        self.TestImage.setPixmap(self.jpg)
+
+    def padding_black(self, img):
+
+        w, h = img.size
+
+        scale = 160. / max(w, h)
+        img_fg = img.resize([int(x) for x in [w * scale, h * scale]]) # 等比压缩
+
+        size_fg = img_fg.size
+        size_bg = 160
+
+        img_bg = Image.new("RGB", (size_bg, size_bg))
+
+        img_bg.paste(img_fg, ((size_bg - size_fg[0]) // 2,
+                              (size_bg - size_fg[1]) // 2))
+
+        img = img_bg
+        return img
+
+    def get_model(self):
+        model = models.resnet101(pretrained=False)
+        fc_inputs = model.fc.in_features
+        model.fc = nn.Linear(fc_inputs, 214)
+        model = model.cuda()
+        # 加载训练好的模型
+        checkpoint = torch.load('model/model_best/best_checkpoint_resnet101.pth.tar')
+        model.load_state_dict(checkpoint['state_dict'])
+        model.eval()
+        return model
+
+    def softmax(self,x):
+        exp_x = np.exp(x)
+        softmax_x = exp_x / np.sum(exp_x, 0)
+        return softmax_x
+
+    def calssify(self):
+        self.img = self.padding_black(self.img)
+        self.img = self.val_tf(self.img)
+        self.img = self.img.view((-1, 3, 160, 160))
+
+        self.img = self.img.cuda()
+
+        pred = self.model(self.img)
+        pred = pred.data.cpu().numpy()[0]
+        score = self.softmax(pred)
+        self.pred_id = np.argmax(score)
+        self.pre_type = self.labels[self.pred_id][0]
+        # self.TestResult.setText(self.pre_type)
+        self.TestResult.insertPlainText(self.pre_type + '\n')
+        # self.textBrowser.setText(self.ture_type)
+        # self.textBrowser_2.setText(self.pre_type == self.ture_type)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = Main()
+
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ garbage.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'GarbageSorting.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_GarbageSortingUI(object):
+    def setupUi(self, GarbageSortingUI):
+        GarbageSortingUI.setObjectName("GarbageSortingUI")
+        GarbageSortingUI.resize(917, 671)
+        GarbageSortingUI.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
+        self.centralwidget = QtWidgets.QWidget(GarbageSortingUI)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.Input = QtWidgets.QPushButton(self.centralwidget)
+        self.Input.setObjectName("Input")
+        self.gridLayout.addWidget(self.Input, 2, 1, 1, 1)
+        self.Output = QtWidgets.QPushButton(self.centralwidget)
+        self.Output.setObjectName("Output")
+        self.gridLayout.addWidget(self.Output, 3, 1, 1, 1)
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 442, 552))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.ShiBieJieGuo = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.ShiBieJieGuo.setGeometry(QtCore.QRect(10, 10, 101, 31))
+        font = QtGui.QFont()
+        font.setFamily("Agency FB")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.ShiBieJieGuo.setFont(font)
+        self.ShiBieJieGuo.setObjectName("ShiBieJieGuo")
+        self.TestResult = QtWidgets.QTextBrowser(self.scrollAreaWidgetContents)
+        self.TestResult.setGeometry(QtCore.QRect(25, 51, 391, 450))
+        self.TestResult.setObjectName("TestResult")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout.addWidget(self.scrollArea, 0, 1, 1, 1)
+        self.scrollArea_2 = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName("scrollArea_2")
+        self.scrollAreaWidgetContents_1 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_1.setGeometry(QtCore.QRect(0, 0, 442, 622))
+        self.scrollAreaWidgetContents_1.setObjectName("scrollAreaWidgetContents_1")
+        self.GarbageSorting = QtWidgets.QLabel(self.scrollAreaWidgetContents_1)
+        self.GarbageSorting.setGeometry(QtCore.QRect(10, 10, 111, 20))
+        font = QtGui.QFont()
+        font.setFamily("Agency FB")
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.GarbageSorting.setFont(font)
+        self.GarbageSorting.setObjectName("GarbageSorting")
+        self.TestImage = QtWidgets.QLabel(self.scrollAreaWidgetContents_1)
+        self.TestImage.setGeometry(QtCore.QRect(20, 90, 401, 481))
+        self.TestImage.setObjectName("TestImage")
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_1)
+        self.gridLayout.addWidget(self.scrollArea_2, 0, 0, 4, 1)
+        GarbageSortingUI.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(GarbageSortingUI)
+        self.statusbar.setObjectName("statusbar")
+        GarbageSortingUI.setStatusBar(self.statusbar)
+
+        self.retranslateUi(GarbageSortingUI)
+        QtCore.QMetaObject.connectSlotsByName(GarbageSortingUI)
+
+    def retranslateUi(self, GarbageSortingUI):
+        _translate = QtCore.QCoreApplication.translate
+        GarbageSortingUI.setWindowTitle(_translate("GarbageSortingUI", "MainWindow"))
+        self.Input.setText(_translate("GarbageSortingUI", "打开图片"))
+        self.Output.setText(_translate("GarbageSortingUI", "识别图片"))
+        self.ShiBieJieGuo.setText(_translate("GarbageSortingUI", "识别结果："))
+        self.GarbageSorting.setText(_translate("GarbageSortingUI", "垃圾分类"))
+        self.TestImage.setText(_translate("GarbageSortingUI", "TextLabel"))
+
